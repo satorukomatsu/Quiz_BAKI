b0081acc9776e50aaa92264820dd20160bfb9d12
satokoma <sk.comiu@gmail.com>
2021-12-13 00:26:06 +0900

    created 10 quiz

--- src/App.tsx
+++ src/App.tsx
@@ -43,6 +43,150 @@ const App = () => {
         correct: false
       }
     ]
+  }, {
+    question: 
+      '神心会空手の創設者・愚地独歩が持つ異名は「○○殺し」でしょう？',
+    answers: [
+      { 
+        text: '熊',
+        correct: false
+      },{
+        text: '虎',
+        correct: true
+      },{
+        text: '人',
+        correct: false
+      },{
+        text: '鮫',
+        correct: false
+      }
+    ]
+  }, {
+    question: 
+      '地下闘技場最大トーナメントで刃牙が決勝で戦った相手は？',
+    answers: [
+      { 
+        text: '愚地独歩',
+        correct: false
+      },{
+        text: 'ジャック・ハンマー',
+        correct: true
+      },{
+        text: '烈海王',
+        correct: false
+      },{
+        text: '渋川剛気',
+        correct: false
+      }
+    ]
+  }, {
+    question: 
+      '鎬昴昇の得意とする技の名前は？',
+    answers: [
+      { 
+        text: '六波返し',
+        correct: false
+      },{
+        text: 'マッハ突き',
+        correct: false
+      },{
+        text: '握撃',
+        correct: false
+      },{
+        text: '紐切り',
+        correct: true
+      }
+    ]
+  }, {
+    question: 
+      '北海道の奥地で刃牙と死闘を繰り広げた多重人格者「ガイア」、もう一人の人格の名前は？',
+    answers: [
+      { 
+        text: 'ノムラ',
+        correct: true
+      },{
+        text: 'ナカムラ',
+        correct: false
+      },{
+        text: 'サトウ',
+        correct: false
+      },{
+        text: 'ヨシダ',
+        correct: false
+      }
+    ]
+  }, {
+    question: 
+      '刃牙の父は勇次郎ですが、祖父の名はなんでしょう？',
+    answers: [
+      { 
+        text: '勇一郎',
+        correct: true
+      },{
+        text: '鬼一郎',
+        correct: false
+      },{
+        text: '力（リキ）',
+        correct: false
+      },{
+        text: '神一',
+        correct: false
+      }
+    ]
+  }, {
+    question: 
+      'グラップラー刃牙シリーズで刃牙が一番最初に戦ったのは誰？',
+    answers: [
+      { 
+        text: '加藤清澄',
+        correct: false
+      },{
+        text: '末堂厚',
+        correct: true
+      },{
+        text: '本部以蔵',
+        correct: false
+      },{
+        text: 'ゲリー・ストライダム',
+        correct: false
+      }
+    ]
+  }, {
+    question: 
+      '刃牙の彼女の本名は？',
+    answers: [
+      { 
+        text: '松本梢江',
+        correct: true
+      },{
+        text: '坂本梢江',
+        correct: false
+      },{
+        text: '朱沢江珠',
+        correct: false
+      },{
+        text: '佐藤梢江',
+        correct: false
+      }
+    ]
+  }, {
+    question: 
+      '次のうち、刃牙と血の繋がりがないのは誰？',
+    answers: [
+      { 
+        text: '範馬勇次郎',
+        correct: false
+      },{
+        text: '朱沢江珠',
+        correct: false
+      },{
+        text: 'ジャック・ハンマー',
+        correct: false
+      },{
+        text: '朱沢鋭一',
+        correct: false
+      }
+    ]
   }
 ]
 
