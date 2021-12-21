e869020d26be889ead77fafa7ba861cba3896927
satokoma <sk.comiu@gmail.com>
2021-12-21 21:52:28 +0900

    youtube, top style done

--- src/component/StartButton.tsx
+++ src/component/StartButton.tsx
@@ -5,8 +5,13 @@ import { NONAME } from "dns";
 
 export const StartButton = () => {
     return (
-        <Button className="startButton" variant="contained" disableElevation>
-            <Link style={{ textDecoration: 'none', color: "white"}} to="/quiz">PUSH TO START</Link>
-        </Button>
+        <div className="button">
+            <Button className="startButton" variant="contained" disableElevation>
+                <Link style={{ textDecoration: 'none', color: "white"}} to="/quiz">PUSH TO START</Link>
+            </Button>
+            <Button className="descriptionButton" variant="contained" disableElevation>
+                <Link style={{ textDecoration: 'none', color: "white"}} to="/description">WHAT'S BAKI</Link>
+            </Button>
+        </div>
     )
 }
--- src/component/start.tsx
+++ src/component/start.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import topImage from '../images/baki_top.jpg'
+import topImage from '../images/Baki-logo.jpg'
 import { StartButton } from './StartButton';
+import { YouTube } from './YouTube';
 
 export const StartImage = () => {
     return (
       <React.Fragment>
         <CssBaseline />
         <Container className="topImage" maxWidth="sm">
-          <Box sx={{ height: '100vh', width: '100vh', margin: '0 auto' }} >
-              <img src={topImage} alt="" />
+            <img src={topImage} alt="topImage" />
+            <h1>クイズゲーム</h1>
+            <Box>
               <StartButton />
             </Box>
         </Container>
