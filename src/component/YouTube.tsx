f6668077983bf92d11de6feab53f0d58fb1f67a9
satokoma <sk.comiu@gmail.com>
2021-12-22 00:39:18 +0900

    add youtube file

---
+++ src/component/YouTube.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import axios from "axios";
+import { useState } from "react";
+
+export const YouTube = () => {
+    const YOUTUBE_API_KEY = ''
+    const BAKI_PLAYLIST_ID = 'PLVs8KIyueUjUuezqLfT1w-37-dhP-AIAR'
+    const url = `https://www.googleapis.com/youtube/v3/playlistItems?key=${YOUTUBE_API_KEY}&playlistId=${BAKI_PLAYLIST_ID}&part=snippet&maxResults=3`
+
+    let [videoId, setVideoId] = useState()
+
+        axios.get(url)
+        .then(response => {
+            let jsonData = response.data
+            setVideoId(
+                videoId = jsonData.items[0].snippet.resourceId.videoId
+            )
+        })
+        .catch(() => {
+            console.log(Error)
+        })
+        
+    return (
+        <>
+            <iframe 
+                width="560"
+                height="315"
+                src={`https://www.youtube.com/embed/${videoId}`}
+                title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" />
+        </>
+    )
+
+    
+}
